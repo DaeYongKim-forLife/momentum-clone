18da766c0bdc617153073038d98c2ce5d8e3c206
han-riang <dy0358@daum.net>
2022-01-21 20:02:21 +0900

    img change
    img change

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ const date = new Date();
 const hour = String(date.getHours()).padStart(2,"0");
 const minute = String(date.getMinutes()).padStart(2,"0");
 const second = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${hour}:${minute}:${second}`;    
+    clock.innerText = `${hour} : ${minute} : ${second}`;    
 }
 
 setInterval(newClock, 1000);
