ad31caa749321dddc514b858003dddf513030aaf
han-riang <dy0358@daum.net>
2022-01-23 15:06:03 +0900

    add the conetent has a quote
    add quote content

--- js/quote.js
+++ js/quote.js
@@ -1,10 +1,10 @@
-const quote = [{ author: "-asd-", content: "asd" },
-    { author: "-fgh-", content: "fgh" },
-    { author: "-zxc-", content: "zxc" }, { author: "-123-", content: "123" }];
+const quote = [{ author: "-Friedrich Nietzsche-", content: "To live is to suffer, to survive is to find some meaning in the suffering." },
+    { author: "-Aristotle-", content: "There is no great genius without some touch of madness." },
+    { author: "-Aristotle-", content: "A friend to all is a friend to none." }, { author: "-William Shakespeare-", content: "A fool thinks himself to be wise, but a wise man knows himself to be a fool." }];
 const spanAuthor = document.querySelector("#quote-author");
 const spanContent = document.querySelector("#quote-content");
 const author = quote.author;
 const quoteNum = Math.floor(Math.random() * quote.length);
-spanAuthor.innerText = quote[quoteNum].author
 spanContent.innerText = quote[quoteNum].content
+spanAuthor.innerText = quote[quoteNum].author
 
